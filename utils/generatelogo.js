34f1213f45e51cdf1b670d3ca6c2c62400df4ac1
Jen Sandage <jensandage1@gmail.com>
2023-07-12 18:30:16 -0700

    fixed svg code, still need to debug shape

--- utils/generatelogo.js
+++ utils/generatelogo.js
@@ -2,23 +2,22 @@ function generateLogo ({characters, textColor, shape, shapeColor}){
     
     function createShape(shape, shapeColor){
         const userInput = "";
-        if(userInput === "circle") {
+        if(userInput === "Circle") {
             return shape = `<circle cx="150" cy="100" r="80" stroke="black" fill="${shapeColor}" stroke-width="5"/>`
-        } else if (userInput === "square") {
+        } else if (userInput === "Square") {
             return shape = `<rect x="90" y="40" width="120" height="120" stroke="black" fill="${shapeColor}" stroke-width="5"/>`    
         } else {
             return shape = `<polygon points="150, 18 244, 182 56, 182" stroke="black" fill="${shapeColor}" stroke-width="5"/>`
          }
         };
 
-       const finishedShape= createShape(shape,shapeColor);
-
+       const finishedShape = createShape(shape,shapeColor);
 
     const svg = 
-`<?xml version="1.0" encoding="UTF-8"?>
-<svg width="300" height="200">
-      <characters fill="${textColor}">${characters}</characters>
-        <shape>${finishedShape}</shape>        
+`
+<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+     ${finishedShape}  
+      <text text-anchor="middle" x="150" y="125" font-size="40" fill="${textColor}">${characters}</text>       
 </svg>
     `
 
@@ -26,3 +25,7 @@ function generateLogo ({characters, textColor, shape, shapeColor}){
 }
 
 module.exports = generateLogo;
+
+//finished shape not populating with anything but the triangle option
+//This XML file does not appear to have any style information associated with it. The document tree is shown below. 
+//above is error getting when opening the svg
