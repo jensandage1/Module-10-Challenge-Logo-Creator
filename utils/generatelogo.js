3e1636ba84650859e2ffdc0dc9fefc7656ad2fe1
Jen Sandage <jensandage1@gmail.com>
2023-07-12 20:51:23 -0700

    trying to get shape to respond to user input

--- utils/generatelogo.js
+++ utils/generatelogo.js
@@ -3,15 +3,15 @@ function generateLogo ({characters, textColor, shape, shapeColor}){
     function createShape(shape, shapeColor){
         const userInput = "";
         if(userInput === "Circle") {
-            return shape = `<circle cx="150" cy="100" r="80" stroke="black" fill="${shapeColor}" stroke-width="5"/>`
+            return shape = `<circle cx="150" cy="100" r="80" stroke="none" fill="${shapeColor}" stroke-width="5"/>`;
         } else if (userInput === "Square") {
-            return shape = `<rect x="90" y="40" width="120" height="120" stroke="black" fill="${shapeColor}" stroke-width="5"/>`    
+            return shape = `<rect x="90" y="40" width="120" height="120" stroke="none" fill="${shapeColor}" stroke-width="5"/>`;    
         } else {
-            return shape = `<polygon points="150, 18 244, 182 56, 182" stroke="black" fill="${shapeColor}" stroke-width="5"/>`
+            return shape = `<polygon points="150, 18 244, 182 56, 182" stroke="none" fill="${shapeColor}" stroke-width="5"/>`;
          }
         };
 
-       const finishedShape = createShape(shape,shapeColor);
+       let finishedShape = createShape(shape,shapeColor);
 
     const svg = 
 `
